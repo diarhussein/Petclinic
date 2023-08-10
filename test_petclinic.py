2339feea47f1f4b52991f12ead8b148c94c84d78
diar.hussein <diar.hussein@devoteam.com>
2023-08-10 13:32:39 +0200

    Final

--- test_petclinic.py
+++ test_petclinic.py
@@ -1,20 +1,37 @@
+import logging
 import time
-import unittest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
+chrome_options = Options()
+#chrome_option.add.argument('--headless')
+driver = webdriver.Chrome(options=chrome_options)
 
-class PetclinicTest(unittest.TestCase):
+# Navigate to the home page
+driver.get("http://localhost:8080/petclinic/")
 
-    def setUp(self):
-        #self.driver = webdriver.Chrome(executable_path=) # Update the path to your driver
+time.sleep(5)
 
-        self.service = Service(executable_path='/usr/local/bin/chromedriver')
-        self.options = webdriver.ChromeOptions()
-        self.driver = webdriver.Chrome(options = self.options)
+# Find and click on the 'Find Owners' link
+find_owners_link = driver.find_element(By.XPATH, '//a[contains(@href, "/petclinic/owners/find")]')
+find_owners_link.click()
 
-    def test_home_page(self):
-        self.driver.get('http://localhost:8080/petclinic/')
-        title = self.driver.title
-        self.assertEqual(title, "Home") # Update with your actual title
+time.sleep(5)
+
+search_bar = driver.find_element(By.NAME, "lastName")
+search_bar.clear()
+search_bar.send_keys("Franklin")
+time.sleep(1)
+search_bar.submit()
+time.sleep(5)
+
+# Find and click on the 'Veterinarians' link
+find_owners_link = driver.find_element(By.XPATH, '//a[contains(@href, "/petclinic/vets")]')
+find_owners_link.click()
+time.sleep(5)
+
+# Find and click on the 'Veterinarians' link
+find_owners_link = driver.find_element(By.XPATH, '//a[contains(@href, "/petclinic/oups")]')
+find_owners_link.click()
+time.sleep(5)
