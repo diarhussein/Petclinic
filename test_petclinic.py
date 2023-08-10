a65ff4b243c82e9d54ffc2ebe6ade79275e4cd42
diar.hussein <diar.hussein@devoteam.com>
2023-08-10 10:25:05 +0200

    Update

--- test_petclinic.py
+++ test_petclinic.py
@@ -18,21 +18,3 @@ class PetclinicTest(unittest.TestCase):
         self.driver.get('http://localhost:8080/petclinic/')
         title = self.driver.title
         self.assertEqual(title, "Petclinic Home Page") # Update with your actual title
-
-    def test_find_owners_page(self):
-        self.driver.get('http://localhost:8080/petclinic/owners/find')
-        # You can add additional checks here, such as searching for an owner and verifying the results
-
-    def test_veterinarians_page(self):
-        self.driver.get('http://localhost:8080/petclinic/vets')
-        # You may want to add checks here to verify specific content on the Veterinarians page
-
-    def test_error_page(self):
-        self.driver.get('http://localhost:8080/petclinic/oups')
-        # Add checks to verify the error message or other content on the error page
-
-    def tearDown(self):
-        self.driver.quit()
-
-if __name__ == "__main__":
-    unittest.main()
