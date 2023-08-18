49d2956135c18382f34d33a4dab2b453fb6ea728
diar.hussein <diar.hussein@devoteam.com>
2023-08-18 11:55:52 +0200

    updated pipeline met docker buld en terraform integratie

--- terraform/main.tf
+++ terraform/main.tf
@@ -108,7 +108,7 @@ resource "azurerm_linux_virtual_machine" "petclinic_vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "20.04-LTS"
     version   = "latest"
   }
 }
