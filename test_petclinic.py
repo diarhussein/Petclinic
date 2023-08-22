2b1b773138cb9c676972cddb6ac2b1ac393778f1
diar.hussein <diar.hussein@devoteam.com>
2023-08-22 13:28:25 +0200

    update

--- test_petclinic.py
+++ test_petclinic.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 import logging
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -16,38 +17,44 @@ def browser():
     driver = webdriver.Chrome(options=chrome_options)
     yield driver
     driver.quit()
+    time.sleep(5)
 
 def test_navigate_home_page(browser):
     logging.info("Navigating to the home page")
     browser.get("http://localhost:8080/petclinic/")
     assert "Expected Title or Content" in browser.page_source
+    time.sleep(5)
 
 def test_find_owners(browser):
     logging.info("Finding and clicking on the 'Find Owners' link")
     find_owners_link = browser.find_element(By.XPATH, '//a[contains(@href, "/petclinic/owners/find")]')
     find_owners_link.click()
     assert "Expected Content" in browser.page_source
+    time.sleep(5)
 
 def test_search_franklin(browser):
     logging.info("Searching for 'Franklin'")
     search_bar = browser.find_element(By.NAME, "lastName")
     search_bar.clear()
     search_bar.send_keys("Franklin")
+    time.sleep(1)
     search_bar.submit()
     assert "Franklin" in browser.page_source
+    time.sleep(5)
 
 def test_navigate_veterinarians(browser):
     logging.info("Finding and clicking on the 'Veterinarians' link")
     vets_link = browser.find_element(By.XPATH, '//a[contains(@href, "/petclinic/vets")]')
     vets_link.click()
     assert "Expected Vet Content" in browser.page_source
+    time.sleep(5)
 
 def test_navigate_oups(browser):
     logging.info("Finding and clicking on the 'oups' link")
     oups_link = browser.find_element(By.XPATH, '//a[contains(@href, "/petclinic/oups")]')
     oups_link.click()
     assert "Expected Oups Content" in browser.page_source
-
+    time.sleep(5)
 
 # import logging
 # import time
